0abaff8716fd039968575cc342f32fb229ed58fb
linan <maura.dust@gmail.com>
2023-08-18 15:30:02 +0300

    route "orai"

--- flaskapp_01.py
+++ flaskapp_01.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "<h1>Cia mano pirmasis home puslapis su Flask</h1>"
-@app.route("/<kintamasis>")
+
+@app.route("/orai")
+def orai():
+    return "<h1>orai per karsti</h1>"
+
+
+@app.route("/<kintamasis>") # dinaminis dalykas
 def user(kintamasis):
     return f"Sveiki {kintamasis} atvykes i Flask page'a"
 
